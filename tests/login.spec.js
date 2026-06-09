148b7abbd5847fa6d9065bdb7d1e6c013665f2fe
RenatoBonettiNeto <bonettineto.renato03@gmail.com>
2026-06-08 21:34:31 -0300

    feat: Configurar teste para caso de fracasso no login onde o usuário está incorreto.

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -18,3 +18,13 @@ test("Positive LogIn test.", async ({ page }) => {
   await expect(page.getByRole("link", { name: "Log out" })).toBeVisible();
   //  await page.getByRole("link", { name: "Log out" }).click();
 });
+
+test("Negative username test", async ({ page }) => {
+  await page.goto("https://practicetestautomation.com/practice-test-login/");
+  await expect(page).toHaveTitle("Test Login | Practice Test Automation");
+  await page.locator("#username").fill("incorrectUser");
+  await page.locator("#password").fill("Password123");
+  await page.locator("#submit").click();
+  await expect(page.locator("#error")).toBeVisible();
+  await expect(page.locator("#error")).toHaveText("Your username is invalid!");
+});
