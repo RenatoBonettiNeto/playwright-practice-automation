3b65cfb423553a83300fd302ca4bb24cc707d726
RenatoBonettiNeto <bonettineto.renato03@gmail.com>
2026-06-08 21:26:08 -0300

    feat: Configurar teste para caso de login positivo.

---
+++ tests/login.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test("Positive LogIn test.", async ({ page }) => {
+  await page.goto("https://practicetestautomation.com/practice-test-login/");
+  await expect(page).toHaveTitle("Test Login | Practice Test Automation");
+  await page.locator("#username").fill("student");
+  await page.locator("#password").fill("Password123");
+  await page.locator("#submit").click();
+  await expect(page).toHaveURL(
+    "https://practicetestautomation.com/logged-in-successfully/",
+  );
+  await expect(page.locator(".post-title")).toHaveText(
+    "Logged In Successfully",
+  );
+  await expect(
+    page.getByText("Congratulations student. You successfully logged in!"),
+  ).toBeVisible();
+  await expect(page.getByRole("link", { name: "Log out" })).toBeVisible();
+  //  await page.getByRole("link", { name: "Log out" }).click();
+});
